50760da27caa2cba10cbc469b34f21d3053b6503
k1747ck <khushikadam4236@gmail.com>
2025-07-28 10:38:10 +0530

    practiced basics in java

--- Learning/Main.java
+++ Learning/Main.java
@@ -1,23 +1,102 @@
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-class Main{
-    public static void main(String[] args) {
-        @SuppressWarnings("resource")
-        Scanner sc = new Scanner(System.in);
+// class Main{
+//     public static void main(String[] args) {
+//         @SuppressWarnings("resource")
+//         Scanner sc = new Scanner(System.in);
         
-        int n = sc.nextInt();
+//         int n = sc.nextInt();
         
-        int [] squares = new int[n];
+//         int [] squares = new int[n];
 
-        for (int i=0; i<n; i++) {
-            squares[i]= (i+1)*(i+1);
+//         for (int i=0; i<n; i++) {
+//             squares[i]= (i+1)*(i+1);
 
-            System.out.println("Square from 1 to " +n+ ":");
-            for (int square : squares){
-                System.out.println(square+" ");
-            }
+//             System.out.println("Square from 1 to " +n+ ":");
+//             for (int square : squares){
+//                 System.out.println(square+" ");
+//             }
             
-        }
-    }
-}
+//         }
+//     }
+// }
+// // practicing basic java consepts
+// // smiple intrest calculator
+
+
+// import java.util.Scanner;
+
+// public class Main{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int p = sc.nextInt();
+//         int r = sc.nextInt();
+//         int t = sc.nextInt();
+
+//         int si = (p*r*t)/100;
+
+//         System.out.println("Simple Interest = " + si);
+//     }
+// }
+
+// // savings formula
+
+// import java.util.Scanner;
+
+// public class Main {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+        
+//         int n = sc.nextInt();
+//         int m = sc.nextInt();
+
+//         int savings = n-m;
+
+//         System.out.println("Savings = " + savings);
+//     }
+// }
+
+
+// // printing numbers
+
+// import java.util.Scanner;
+
+// public class Main {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int a = 10;
+//         int b = 20;
+//         int c = 30;
+
+//         System.out.println(a);
+//         System.out.println(b);
+//         System.out.println(c);
+
+//     }
+// }
+
+// // Arithmetic Operators
+//  public class Main {
+ 
+//     public static void main(String[] args) {
+//         int a = 5;
+//         int b = 4;
+
+//         int c = a + b;
+//         System.out.println("Sum =" + c);
+
+//         System.out.println("Addition = " + (a + b));
+//         System.out.println("Subtraction = " + (a - b));
+//         System.out.println("Division = " + (a / b));
+//         System.out.println("Multiplication = " + (a * b));
+
+//     }
+//  }
+
+
+ 
