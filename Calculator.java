3c4faa20625a51b28edabd1f3475f50a01173595
k1747ck <khushikadam4236@gmail.com>
2025-07-31 16:47:41 +0530

    initial commit

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first value:");
+        int a = sc.nextInt();
+        
+
+    }
+}
---
+++ Index.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+class Index {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the value of a = ");
+
+        int a = sc.nextInt();
+
+        System.out.println("the value of a is = " + a);
+    }
+}
