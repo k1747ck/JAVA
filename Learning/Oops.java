c12df2a4f36bb62cc5c77e0c07ac0843c07ade5a
k1747ck <khushikadam4236@gmail.com>
2025-08-06 14:45:02 +0530

    simple calculator

--- Learning/Oops.java
+++ Learning/Oops.java
@@ -191,52 +191,53 @@
 // }
 
 // // 3 D array
-public class Oops
-{
-    public static void main(String[] args) 
-    {
-        int nums[][][] = new int[3][4][5]; // 3 dimentional array
+// public class Oops
+// {
+//     public static void main(String[] args) 
+//     {
+//         int nums[][][] = new int[3][4][5]; // 3 dimentional array
 
        
         
 
-        for (int i=0;i<nums.length;i++)
-        {
-            for(int j=0;j<nums[i].length;j++)
-            {
-                for(int k=0;k<nums[i][j].length;k++)
-                {
-                 nums[i][j][k] = (int)(Math.random()*10);
-                }
+//         for (int i=0;i<nums.length;i++)
+//         {
+//             for(int j=0;j<nums[i].length;j++)
+//             {
+//                 for(int k=0;k<nums[i][j].length;k++)
+//                 {
+//                  nums[i][j][k] = (int)(Math.random()*10);
+//                 }
                 
-            }
+//             }
         
-        }
+//         }
 
 
-        for (int i=0;i<nums.length;i++)
-        {
-            for(int j=0;j<nums[i].length;j++)
-            {
-                 for(int k=0;k<nums[i][j].length;k++)
-                 {
-                    System.out.println( nums[i][j][k] + " ");
-            }
-            System.out.println();
-            }
-            System.out.println();
-        }
+//         for (int i=0;i<nums.length;i++)
+//         {
+//             for(int j=0;j<nums[i].length;j++)
+//             {
+//                  for(int k=0;k<nums[i][j].length;k++)
+//                  {
+//                     System.out.println( nums[i][j][k] + " ");
+//             }
+//             System.out.println();
+//             }
+//             System.out.println();
+//         }
 
-        for (int n[][]: nums)
-        {
-            for (int m[]: n)
-            {
-                for(int k: m)
-                {
-                    System.out.print(k + " ");
-                }
-            System.out.println();
-            }
-        }
-    }
-}
+//         for (int n[][]: nums)
+//         {
+//             for (int m[]: n)
+//             {
+//                 for(int k: m)
+//                 {
+//                     System.out.print(k + " ");
+//                 }
+//             System.out.println();
+//             }
+//         }
+//     }
+// }
+
--- Ubuntu-progs/Calculator.java
+++ Ubuntu-progs/Calculator.java
@@ -5,6 +5,46 @@ public class Calculator {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter first value:");
-        int a = sc.nextInt();
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter the operator (+,-,*,/):");
+        char operator = sc.next().charAt(0);
+
+        System.out.println("Enter second value:");
+        int num2 = sc.nextInt();
+
+        int result;
+
+        switch (operator) {
+            case '+' :
+                result = num1 + num2;
+                System.out.println("Result" + result);
+                break;
+            
+            case '-' :
+                result = num1 - num2;
+                System.out.println("Result" + result);
+                break;
+
+            case '*' :
+                result = num1 * num2;
+                System.out.println("Result" + result);
+                break;
+                
+            case '/' :
+                if(num2 !=0 ){
+                result = num1 / num2;
+                System.out.println("Result" + result);
+                }
+                else {
+                    System.out.println("Error!!! Cannot divide by 0");
+                }
+                break;
+
+            default:
+                System.out.println("Invalid operator!!! ");    
+        }
+
+    sc.close();
     }
 }
